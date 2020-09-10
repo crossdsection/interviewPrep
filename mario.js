de99101bc84445dc0a991170e6c3a8647b484dca
Piyush Kumar <chirkut.kr@gmail.com>
2020-09-10 19:59:03 +0530

    Exercise 2 drawPyramid

--- mario.js
+++ mario.js
@@ -27,4 +27,25 @@ function printPyramid(height) {
         console.log(str);
         firstSharpCounter++;
     }
+    drawPyramid(height);
+}
+
+function drawPyramid(height) {
+    firstSharpCounter = 2;
+    for( var j = 0; j < height; j++ ) {
+        var str = '';
+        for( var i = height; i >= 0; i--) {
+            if( i < firstSharpCounter ) {
+                str+= '<div style="display:inline-block;width:40px;height:40px;background:#820000;border:2px solid;border-color:white;"></div>';
+            } else {
+                str+= '<div style="display:inline-block;width:40px;height:40px;background:white;border:2px solid;border-color:white;"></div>';
+            }
+        }
+        var para = document.createElement("p");
+        para.style.margin = "0px";
+        para.innerHTML = str;
+        document.getElementById("pyramid").appendChild(para);
+        firstSharpCounter++;
+    }
+    document.getElementById("pyramid").removeChild(document.getElementById("construction"));
 }
