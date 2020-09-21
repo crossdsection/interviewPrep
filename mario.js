8a900449d4ce9e486c37365d16e3076c7ff43c83
Piyush Kumar <chirkut.kr@gmail.com>
2020-09-22 04:13:35 +0530

    Mario blocks using Jquery

--- mario.js
+++ mario.js
@@ -1,51 +1,47 @@
 
-printPyramid(5);
+$(document).ready(function(){
+    printPyramid($("#bricksCount").val());
+    $("#symbol").on('change', drawPyramid);
+    $("#bricksCount").on('change', drawPyramid);
 
-
-/*
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
-function printPyramid(height) {
-    firstSharpCounter = 2;
-    for( var j = 0; j < height; j++ ) {
-        var str = '';
-        for( var i = height; i >= 0; i--) {
-            if( i < firstSharpCounter ) {
-                str+= '#';
-            } else {
-                str+= ' ';
+    function printPyramid(height) {
+        firstSharpCounter = 2;
+        for( var j = 0; j < height; j++ ) {
+            var str = '';
+            for( var i = height; i >= 0; i--) {
+                if( i < firstSharpCounter ) {
+                    str+= '#';
+                } else {
+                    str+= ' ';
+                }
             }
+            firstSharpCounter++;
         }
-        console.log(str);
-        firstSharpCounter++;
+        drawPyramid(height);
     }
-    drawPyramid(height);
-}
 
-function drawPyramid(height, symbol = 'box') {
-    document.getElementById("pyramid").innerHTML = "";
-    firstSharpCounter = 2;
-    for( var j = 0; j < height; j++ ) {
-        var str = '';
-        for( var i = height; i >= 0; i--) {
-            if( i < firstSharpCounter ) {
-                str+= (symbol == 'box' ) ? '<div style="display:inline-block;width:40px;height:40px;background:#820000;border:2px solid;border-color:#eeeeee;"></div>' : symbol;
-            } else {
-                str+= (symbol == 'box' ) ? '<div style="display:inline-block;width:40px;height:40px;background:#eeeeee;border:2px solid;border-color:#eeeeee;"></div>' : "&nbsp;";
+    function drawPyramid(height, symbol = 'box') {
+        height = $("#bricksCount").val();
+        symbol = $("#symbol").val();
+
+        $("#pyramid").html('');
+        firstSharpCounter = 2;
+        for( var j = 0; j < height; j++ ) {
+            var str = '';
+            for( var i = height; i >= 0; i--) {
+                if( i < firstSharpCounter ) {
+                    str+= (symbol == 'box' ) ? '<div style="display:inline-block;width:40px;height:40px;background:#820000;border:2px solid;border-color:#eeeeee;"></div>' : symbol;
+                } else {
+                    str+= (symbol == 'box' ) ? '<div style="display:inline-block;width:40px;height:40px;background:#eeeeee;border:2px solid;border-color:#eeeeee;"></div>' : "&nbsp;";
+                }
             }
+            let para = $("<p>");
+            para.css({margin:0});
+            console.log(para);
+            $("#pyramid").append(para);
+            para.html(str);
+            firstSharpCounter++;
         }
-        var para = document.createElement("p");
-        para.style.margin = "0px";
-        para.innerHTML = str;
-        document.getElementById("pyramid").appendChild(para);
-        firstSharpCounter++;
     }
-}
+
+});
